e5320ec27ca1a8b8b2db301309dd86e160a5393a
Lindsay Drozdik <linds.drozdik@yahoo.com>
2024-02-10 15:21:30 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Sam",
+    streetAddress: "12 Broadway",
+};
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const updatedEmployee = {
+        ...obj,
+        [key]: value,
+    };
+    return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+
+  return obj;
+}
+function deleteFromEmployeeByKey(obj, key, value) {
+    const updatedEmployee = {
+       key: value,
+    };
+    delete key.value;
+    return updatedEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
